3ecc548539bccc9b4c1eb7cb60fc01dc7ddbe82f
ruffineli77 <ruffineli77@gmail.com>
2024-10-12 06:25:32 -0400

    Backend can receive contact for information and save a message and sender record. Temporarily the Message model uses a default value for the message_sender field. Without the default value a 400 error is returned stating the message_senderfield is required. Logging will be added next to determine the solution.

--- backend/models.py
+++ backend/models.py
@@ -12,7 +12,7 @@ class Sender(models.Model):
 class Message(models.Model):
     message = models.CharField(max_length=200, default="")
     message_sender = models.ForeignKey(
-        Sender, on_delete=models.CASCADE,  default=1
+        Sender, on_delete=models.CASCADE,
     )  # A primary key NOT a string/email
     date = models.DateTimeField(auto_now_add=True, blank=True)
 
